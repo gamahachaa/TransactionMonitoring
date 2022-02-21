7f6ab23fbab6664d1fe456e6499f424920b41867
bbaudry <bruno.baudry@salt.ch>
2022-02-21 15:25:35 +0100

    TM with markdown and starting sub summaries

---
+++ src/ui/dialogs/MarkdownHelp.hx
@@ -0,0 +1,18 @@
+package ui.dialogs;
+
+import haxe.ui.containers.dialogs.MessageBox;
+
+/**
+ * ...
+ * @author bb
+ */
+class MarkdownHelp extends MessageBox 
+{
+
+	public function new() 
+	{
+		super();
+		
+	}
+	
+}
